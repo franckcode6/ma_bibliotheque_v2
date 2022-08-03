b3678d68b28ab8045cdd2f83c989a59fdd99d4fd
Franck Quasevi <franckcode6@gmail.com>
2022-08-03 16:00:06 +0200

    Creation du projet sous IntelliJ

---
+++ src/main/java/fr/franck/ma_bibliotheque_v2/MaBibliothequeV2Application.java
@@ -0,0 +1,13 @@
+package fr.franck.ma_bibliotheque_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaBibliothequeV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaBibliothequeV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/franck/ma_bibliotheque_v2/MaBibliothequeV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.franck.ma_bibliotheque_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaBibliothequeV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
