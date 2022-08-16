0e22fd09ed26a357ab81b71d21c3fae743f869a9
Franck Quasevi <franckcode6@gmail.com>
2022-08-16 10:29:32 +0200

    Creation du fichier de filtre

---
+++ src/main/java/fr/franck/ma_bibliotheque_v2/filter/CheckSessionFilter.java
@@ -0,0 +1,35 @@
+package fr.franck.ma_bibliotheque_v2.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckSessionFilter implements Filter {
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		if (((HttpServletRequest) request).getRequestURI().startsWith("/admin")
+				&& ((HttpServletRequest) request).getSession().getAttribute("admin") == null) {
+			System.out.println("Pas de session");
+			((HttpServletResponse) response).sendRedirect("/livres");
+		} else if (((HttpServletRequest) request).getRequestURI().startsWith("/compte")
+				&& (((HttpServletRequest) request).getSession().getAttribute("lecteurConnecte")) == null) {
+			System.out.println("Pas de session");
+			((HttpServletResponse) response).sendRedirect("/livres");
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+}
